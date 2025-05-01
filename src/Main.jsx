921b03448ec96756ad571e4e71a971db17e91a2c
Saheim <80918419+Sah31m@users.noreply.github.com>
2025-05-01 08:16:22 +0100

    initupld

--- src/Main.jsx
+++ src/Main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <BrowserRouter>
-      <Routes>
+      <Routes basename="/Sah31m-Portfolio/">
         <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />}/>
             <Route path="Projects" element={<Projects />}/>
